568ba3280e1b31a571a1cd45a2835ddbe6a6eebe
tehrimzahra <tehrimzahra984@gmail.com>
2024-08-03 10:41:44 +0500

    first commit

---
+++ class3.py
@@ -0,0 +1,8 @@
+
+s = "hello, World!"
+capitalized = s.capitalize()
+print(f"Capitalized: {capitalized}")
+uppercase = s.upper()
+print(f"Uppercase: {uppercase}")
+lowercase = s.lower()
+print(f"Lowercase: {lowercase}")
